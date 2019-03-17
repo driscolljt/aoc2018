f450fd8c00ace063322f10d2c6934b3ddf89b8b6
jdriscoll <jdriscoll.dev@gmail.com>
2019-03-17 13:42:11 -0700

    changing my approach to this challenge

--- day6/p1/main.go
+++ day6/p1/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"math"
 	"sort"
@@ -24,97 +23,31 @@ type dist struct {
 
 func main() {
 	input := data()
-	data := sortInput(input)
+	input = sortInput(input)
 
-	for i := range data {
-		distances := make([]dist, len(data)-1)
-		for j, d2 := range data {
-			if len(data) > j+1 {
-				distances[j] = calcMnhtnDist(data[i], d2)
+	for i := range input {
+		distances := make([]dist, len(input)-1)
+		for j, d2 := range input {
+			if len(input) > j+1 {
+				distances[j] = calcDistance(input[i], d2)
 			}
 		}
-		// do calculations at this point using the distances acquired
-		// to determine if this set has a greater region than the previous
-		// sort the largest region to the top and print
-		var ul dist
-		var ur dist
-		var bl dist
-		var br dist
+		// at this point in the loop
+		// i have the distances from the current
+		// data point to every other point
 
-		for _, di := range distances {
-			u := false
-			r := false
-			l := false
-			b := false
-			if (di.xDist - data[i].x) > 0 {
-				r = true
-			} else if (di.xDist - data[i].x) < 0 {
-				l = true
-			}
-			if (di.yDist - data[i].y) > 0 {
-				u = true
-			} else if (di.yDist - data[i].y) < 0 {
-				b = true
-			}
+		// if the current point has an infinite area,
+		// continue the loop
 
-			var tmpX *int
-			var tmpY *int
+		// otherwise, determine how many points are closer
+		// to the current position than any other
 
-			if u && r {
-				tmpX = &ur.xDist
-				tmpY = &ur.yDist
-			} else if u && l {
-				tmpX = &ul.xDist
-				tmpY = &ul.yDist
-			} else if b && r {
-				tmpX = &br.xDist
-				tmpY = &br.yDist
-			} else if b && l {
-				tmpX = &bl.xDist
-				tmpY = &bl.yDist
-			}
-
-			if tmpX != nil && tmpY != nil {
-				if (*tmpX == 0 && *tmpY == 0) || (*tmpX+*tmpY) > (di.xDist+di.yDist) {
-					*tmpX = di.xDist
-					*tmpY = di.yDist
-				}
-			}
-			// keep only the distances that have finite areas
-			if ul.xDist != 0 && ul.yDist != 0 &&
-				ur.xDist != 0 && ur.yDist != 0 &&
-				bl.xDist != 0 && bl.yDist != 0 &&
-				br.xDist != 0 && br.yDist != 0 {
-
-				fmt.Print("l1")
-				fmt.Print(di.l1)
-				fmt.Println("")
-
-				fmt.Print("l2")
-				fmt.Print(di.l2)
-				fmt.Println("")
-
-				fmt.Print("ur")
-				fmt.Print(ur)
-				fmt.Println("")
-
-				fmt.Print("ul")
-				fmt.Print(ul)
-				fmt.Println("")
-
-				fmt.Print("br")
-				fmt.Print(br)
-				fmt.Println("")
-
-				fmt.Print("bl")
-				fmt.Print(bl)
-				fmt.Println("")
-			}
-		}
+		// keep only the highest and when the loop is complete,
+		// print the point details and the area (including the point itself)
 	}
 }
 
-func calcMnhtnDist(v1, v2 location) dist {
+func calcDistance(v1, v2 location) dist {
 	dX := math.Abs(float64(v1.x) - float64(v2.x))
 	dY := math.Abs(float64(v1.y) - float64(v2.y))
 
